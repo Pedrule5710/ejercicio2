d664f752ac9f71a11f2733c77352b8b6845eab9b
Pedrule5710 <m.r.pedrule@gmail.com>
2024-06-05 13:29:16 +0200

    SUbimos de to

--- src/ejercicio2/AnalisisEdad.java
+++ src/ejercicio2/AnalisisEdad.java
@@ -1,8 +1,41 @@
 package ejercicio2;
 
+import java.util.ArrayList;
+
 public class AnalisisEdad {
-	public static void main(String [] args) {
-		System.out.println("Alo poli");
-	}
+    ArrayList<Integer> edades;
+/**
+ * Constructor edades
+ * @param edades Arraynumeros enteros que contendra las edades
+ * 
+ */
+    public AnalisisEdad(ArrayList<Integer> edades) {
+        this.edades = edades;
+    }
+/**
+ * Metodo para comprobar las edades
+ * @param edades
+ * @return miaumiua
+ */
+    public String comprobar(ArrayList<Integer> edades) {
+        int menores = 0;
+        int mayores = 0;
+        int jubilados = 0;
+
+        for (int i=0; i< edades.size(); i++) {
+            if (edades.get(i) < 18) {
+                menores++;
+            } else if (edades.get(i) > 65) {
+                jubilados++;
+                mayores++;
+            } else {
+                mayores++;
+            }
+        }
+
+        String mensaje = "Menores de edad: " + menores + ", Mayores de edad: " + mayores + ", Jubilados: " + jubilados;
+        return mensaje;
+    }
+
 
 }
--- src/ejercicio2/prinvipa.java
+++ src/ejercicio2/prinvipa.java
@@ -1,10 +1,25 @@
 package ejercicio2;
 
+import java.util.ArrayList;
+/**
+ * Clase principal que contiene el menu o algo que ns en verdad que contiene pero esto se alarga por los jajas porque sabes tu que guapo va a estar si alguien tiene que leer esto encima sin ninguna coma ni mierdas
+ */
 public class prinvipa {
+	/**
+	 * 
+	 * @param args miau miau
+	 */
+    public static void main(String[] args) {
+        ArrayList<Integer> edades = new ArrayList<>();
+        edades.add(10);
+        edades.add(20);
+        edades.add(70);
+        edades.add(15);
+        edades.add(30);
+        edades.add(65);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        AnalisisEdad analisis = new AnalisisEdad(edades);
 
-	}
-
+        System.out.println(analisis.comprobar(edades));
+    }
 }
